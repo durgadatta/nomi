41aad8b3745882c16ee8d38ccebd23c0e8ae2209
Durga Datta Kandel <durgadatta.np@gmail.com>
2025-10-23 14:15:15 -0400

    add a corner case - to be fixed in next commit

--- prototype/tests/data/sample_sources/interpreter/special_cases.py
+++ prototype/tests/data/sample_sources/interpreter/special_cases.py
@@ -3,6 +3,8 @@
 ensure that YieldException is not generically handled in
 eval loop, and also specifically in eval_Try
 '''
+from contextlib import contextmanager
+
 def error_printer_context():
     try:
        yield 1
@@ -11,3 +13,22 @@ def error_printer_context():
 a = list(error_printer_context())
 print(a)
 
+
+
+'''
+ensure that the exception raise inside the 
+while block is passed to the context manger 
+
+see more comments on GeneratorState class
+'''
+
+@contextmanager
+def error_printer_context():
+    try:
+       yield 1
+    except Exception as e:
+       print(f"An error occurred: {e}")
+
+test_value = 1
+with error_printer_context():
+   test_value = 1/0
