ed01d87bf561b07b70e081576cbc8e393eba2da2
Durga Datta Kandel <durgadatta.np@gmail.com>
2025-11-27 13:51:37 -0500

    add resumable function-call

--- prototype/interpreter/python/function_call.py
+++ prototype/interpreter/python/function_call.py
@@ -5,32 +5,96 @@ thus, separating this into a new file
 import ast
 from typing import Any
 
+from .base import YieldException
+
 class FunctionCallMixin:
-    def eval_Call(self, node: ast.Call) -> Any:
-        func = self.eval(node.func)
-        # Evaluate arguments
-        posargs = []
-        for arg in node.args:
-            if isinstance(arg, ast.Starred):
-                posargs.extend(self.eval(arg.value))
-            else:
-                posargs.append(self.eval(arg))
+    def eval_Call(self, node: ast.Call, *, state=None, generator_state=None) -> Any:
+        """
+        Evaluate a function call, supporting yields in arguments.
+        """
+        
+        if state is not None:
+            # Resuming from a yield in arguments
+            func, evaluated_args, next_arg_index = state
+            sent_value = generator_state.get_sent_value() if generator_state else None
+            
+            # Replace the yield with sent value and continue
+            return self._continue_call_evaluation(node, func, evaluated_args, next_arg_index, sent_value, generator_state)
+        
+        else:
+            # First time - start fresh
+            func = self.eval(node.func, generator_state=generator_state)
+            return self._continue_call_evaluation(node, func, [], 0, None, generator_state)
 
+    def _continue_call_evaluation(self, node, func, evaluated_args, start_index, sent_value, generator_state):
+        """
+        Continue evaluating call arguments from start_index.
+        """
         kwargs = {}
-        for kw in node.keywords:
-            if kw.arg is None:
-                kw_val = self.eval(kw.value)
-                if not isinstance(kw_val, dict):
-                    raise TypeError(f"argument after ** must be a mapping at line {self.get_lineno(node)}")
-                kwargs.update(kw_val)
-            else:
-                value = kw.value
-                # if it is a block arg; don't eval it
-                # generator state will eval it in the caller's env
-                if kw.arg == '__block__':
-                    value = (*value, self.current_env)
+        
+        # Process positional arguments
+        for i in range(start_index, len(node.args)):
+            arg = node.args[i]
+            
+            # If we have a sent_value for this position (resuming from yield)
+            if i == start_index and sent_value is not None:
+                evaluated_args.append(sent_value)
+                continue
+                    
+            try:
+                if isinstance(arg, ast.Starred):
+                    starred_val = self.eval(arg.value, generator_state=generator_state)
+                    evaluated_args.extend(starred_val)
                 else:
-                    value = self.eval(kw.value)
+                    evaluated_args.append(self.eval(arg, generator_state=generator_state))
+            except YieldException as ye:
+                # Pause at this argument index
+                state = (func, evaluated_args, i)
+                if generator_state:
+                    generator_state.pause(node, state)
+                raise YieldException(ye.value)
+        
+        # Process keyword arguments
+        kw_start_index = max(0, start_index - len(node.args))
+        for kw_index, kw in enumerate(node.keywords[kw_start_index:], kw_start_index):
+            # If we have a sent_value for this keyword (resuming from yield)
+            if len(node.args) + kw_index == start_index and sent_value is not None:
+                if kw.arg == '__block__':
+                    # Handle __block__ specially
+                    value = (*sent_value, self.current_env)
+                else:
+                    value = sent_value
                 kwargs[kw.arg] = value
+                continue
+                
+            try:
+                if kw.arg is None:
+                    # **kwargs
+                    kw_val = self.eval(kw.value, generator_state=generator_state)
+                    if not isinstance(kw_val, dict):
+                        raise TypeError(f"argument after ** must be a mapping at line {self.get_lineno(node)}")
+                    kwargs.update(kw_val)
+                else:
+                    # __block__ special handling - DON'T evaluate the value
+                    if kw.arg == '__block__':
+                        value = kw.value  # Keep the raw AST node
+                        value = (*value, self.current_env)  # Create tuple with raw node + env
+                    else:
+                        value = self.eval(kw.value, generator_state=generator_state)
+                    kwargs[kw.arg] = value
+            except YieldException as ye:
+                # Pause at keyword argument
+                state = (func, evaluated_args, len(node.args) + kw_index)
+                if generator_state:
+                    generator_state.pause(node, state)
+                raise YieldException(ye.value)
+        
+        result = func(*evaluated_args, **kwargs)
 
-        return func(*posargs, **kwargs)
+        #NOTE this is an abuse of mechanism used in for send() to 
+        # communicate with assignment
+        # review better way to deal with this
+        if generator_state:
+            generator_state.sent_value = result 
+        # All arguments evaluated - call the function
+        return result
